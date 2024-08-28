7390aae0828b6692352de772c643a46dd82e6551
vishal-shingare <vishalshingare29@gmail.com>
2024-08-28 15:01:48 +0530

    [feat] : added passkey modal component and integrate with mail page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { PatientForm } from '@/components/forms/PatientForm';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+import { PatientForm } from '@/components/forms/PatientForm';
+import { PasskeyModal } from '@/components/PasskeyModal';
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === 'true';
+
   return (
     <div className='flex h-screen max-h-screen'>
-      {/* TODO: OTP Verfication | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
+
       <section className='remove-scrollbar container my-auto'>
         <div className='sub-container max-w-[496px]'>
           <Image
@@ -20,16 +25,17 @@ export default function Home() {
 
           <div className='text-14-regular mt-20 flex justify-between'>
             <p className='justify-items-end text-dark-600 xl:text-left'>
-              © 2024 CarePlus
+              © 2024 CarePluse
             </p>
-            <Link href={'/?admin-true'} className='text-green-500'>
+            <Link href='/?admin=true' className='text-green-500'>
               Admin
             </Link>
           </div>
         </div>
       </section>
+
       <Image
-        src={'/assets/images/onboarding-img.png'}
+        src='/assets/images/onboarding-img.png'
         height={1000}
         width={1000}
         alt='patient'
@@ -37,4 +43,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
